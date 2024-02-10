cee5f5764ec754399b205490a92eee2eac55a760
emmaculate-exceel <emmaculate2exceel@live.com>
2024-02-10 14:45:42 +0100

    consoleApp.e

--- console.py
+++ console.py
@@ -4,6 +4,11 @@ console file
 for the console app
 programmes
 """
-
-
-print("Hi Users !!!")
+def greetings():
+    """
+    the is an introductory
+    message to all
+    our users of the console App
+    """
+    return print("Hi Users!!!")
+greetings()
