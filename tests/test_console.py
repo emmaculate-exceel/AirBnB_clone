16b038c9abc9304b7a1c9e43c8c9d4dac2686935
emmaculate-exceel <emmaculate2exceel@live.com>
2024-02-10 12:17:14 +0100

    all

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python3
 #AirBnB test script for the AirBnB project
+
+import unittest
+"""
+Testcase for the console
+and for the AirBnB
+clone website
+"""
+
+class Test_console():
+    """
+    function to check for in
+    the Base model
+    """
+    pass
