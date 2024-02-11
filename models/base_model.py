e4f11dda3d039215936eb623461e9df503bb287c
emmaculate-exceel <emmaculate2exceel@live.com>
2024-02-11 05:31:47 +0100

    update.datas"git push

--- models/base_model.py
+++ models/base_model.py
@@ -1,2 +1,47 @@
 #!/usr/bin/python3
-#Base model Objects for the AirBnB task
+import uuid
+from datetime import datetime
+
+"""
+Basemodel that defines attributes and
+methods for the base class
+"""
+
+
+class BaseModel:
+    """
+    function instantiation of
+    attributes
+    """
+
+    def __init__(self, id, created_at, updated_at):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+    """
+    function to print out 
+    string value of attributes
+    """
+
+    def __str__(self):
+        x = "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return x
+    """
+    updating the current
+    instance of upadated_at
+    using the save() function
+    """
+
+    def save(self):
+        self.updated_at = datetime.now()
+    """
+    setting instance that
+    will return
+    """
+
+    def to_dict(self):
+        objdict = self.__dict__.copy
+        objdict["__class__"] = self.__class__.__name__
+        objdict["created_at"] = self.datetime.isoformat()
+        objdict["updated_at"] = self.datetime.isoformat()
+        return objdict
